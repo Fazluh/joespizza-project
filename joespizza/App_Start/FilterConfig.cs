79f5f390b9d79d81071558db15d3d92b8e5e1c73
Fazluh <fk368414@gmail.com>
2023-12-13 11:26:51 +0530

    Add project files.

---
+++ joespizza/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace joespizza
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ joespizza/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace joespizza.Models
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+
+
+        public string Type { get; set; }
+
+        public int Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
